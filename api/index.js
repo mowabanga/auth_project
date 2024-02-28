7acfc2480d570999c6d9f0e2ac8f3049d473c1fe
mowabanga <itsmundia@gmail.com>
2024-02-28 17:21:47 +0300

    Fixed a critical typo

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
-import path from path
+import path from "path";
 
 dotenv.config();
 mongoose
@@ -22,9 +22,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(express.json());
 
